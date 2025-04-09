919bc240aa90c9fa3606ee43bbd3f14fc0b09556
Roberto Gabriel <roberto.gabriel2004@hotmail.com>
2025-04-08 22:05:50 -0300

    terminando os temas e ajustando o responsivo

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const FotoPerfil = styled.img`
   max-width: 100%;
+  height: 128px;
   border-radius: 50%;
   margin-bottom: 24px;
 `
--- src/styles.ts
+++ src/styles.ts
@@ -9,13 +9,15 @@ const EstiloGlobal = createGlobalStyle`
 }
 
 body {
-    padding-top: 80px;
-    padding-bottom: 80px;
+  padding-top: 80px;
+  padding-bottom: 80px;
+  background-color: ${(props) => props.theme.corDeFundo};
 
-    @media (max-width: 768px) {
-    padding-top:16px
-    }
+  @media (max-width: 768px) {
+    padding-top: 16px;
+  }
 }
+
 
 
 `
