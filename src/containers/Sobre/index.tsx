3ecbbc3a83d94de056fa5ad00ce89ba2de82cb86
Roberto Gabriel <117497923+rbtzinn@users.noreply.github.com>
2025-06-03 15:15:48 -0300

    Update index.tsx

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,18 +6,10 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
-      Engenheiro Front-End em formação pela EBAC – Escola Britânica de Artes
-      Criativas e Tecnologia, graduando em Ciência da Computação pela
-      UNINASSAU-Recife, com inglês intermediário. Profissional com perfil
-      proativo e analítico. Participei do desenvolvimento de um site
-      institucional para a ETE Chico Science, aplicando princípios de design
-      responsivo e boas práticas de UI/UX. Além disso, elaborei o front-end de
-      um e-commerce de calçados, utilizando display grid e estilização
-      sofisticada, e desenvolvi um protótipo de chat interativo, com barra de
-      pesquisa para contatos e sistema de upload/download de arquivos.
-      Atualmente, aprimoro minhas habilidades em desenvolvimento de interfaces e
-      interatividade web, buscando sempre criar aplicações eficientes e
-      visualmente atraentes.
+      Engenheiro Front-End em formação pela EBAC – Escola Britânica de Artes Criativas e Tecnologia, graduando em Ciência da Computação pela UNINASSAU-Recife, com inglês intermediário. 
+      Profissional com perfil proativo, analítico e comprometido com a construção de soluções web eficientes, responsivas e bem estruturadas. Em constante evolução técnica, busca aplicar 
+      boas práticas de desenvolvimento front-end, com foco em usabilidade, performance e organização de código. Está sempre alimentando seu portfólio com projetos relevantes, reforçando seu 
+      aprendizado prático e demonstrando evolução contínua. Demonstra facilidade de aprendizado, interesse por novas tecnologias e dedicação ao aprimoramento constante de suas habilidades.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=rbtzinn&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
